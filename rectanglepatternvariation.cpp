641a22299cb0da4a6283db5d9a28471b0d0f49fa
asheesh20 <98988175+asheesh20@users.noreply.github.com>
2022-06-27 04:53:29 +0530

    Add files via upload

---
+++ rectanglepatternvariation.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#define ll long long 
+using namespace std;
+void rectanglepatternvariation(ll row,ll col){
+	ll i,j;
+	for(i=1;i<=row;i++){
+		for(j=1;j<=col;j++){
+			if(j<=6-i||j>=4+i) cout<<"*";
+			else cout<<" ";
+		}
+		cout<<endl;
+	}
+}
+int32_t main(){
+	rectanglepatternvariation(5,9);
+}
